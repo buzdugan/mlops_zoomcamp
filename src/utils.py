a7af9d7dc1d158cdc6198ff481c059410f97a29a
“buzdugan” <andradaorbai@yahoo.com>
2025-08-01 14:25:44 +0100

    Remove print

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,6 @@ def load_config(file_path="config.yaml"):
     with open(file_path) as stream:
         try:
             config = yaml.safe_load(stream)
-            print(config)
         except yaml.YAMLError as exc:
             print(exc)
 
