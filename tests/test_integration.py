23c23e64653d87d79e8742d4eea454ee5eb3a452
“buzdugan” <andradaorbai@yahoo.com>
2025-08-01 15:49:43 +0100

    Add integration test

---
+++ tests/test_integration.py
@@ -0,0 +1,53 @@
+import os
+from datetime import datetime, timedelta
+from pathlib import Path
+
+import boto3
+from botocore.exceptions import ClientError
+
+import sys
+sys.path.append("src")
+sys.path.append("deployment")
+import utils
+import scoring
+
+
+def check_s3_file_exists(bucket_name, object_key):
+    s3 = boto3.resource('s3')
+
+    try:
+        s3.Object(bucket_name, object_key).load()
+    except ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            return False
+        else:
+            raise
+    else:
+        return True
+
+
+def TestIntegration():
+    config = utils.load_config(file_path="config.yaml")
+    bucket_name = config['bucket_name']
+    os.environ["AWS_PROFILE"] = config['profile_name']
+
+    # Define the input and output file paths
+    yesterday = datetime.now() - timedelta(1)
+    yesterday_str = yesterday.strftime('%Y_%m_%d')
+    scored_data_path_prefix = Path(config['scored_data_path_prefix'])
+    output_file_path = Path(f"{scored_data_path_prefix}_{yesterday_str}.csv")
+
+    scoring.score_claim_status()
+
+    scoring_output_exists = check_s3_file_exists(
+        bucket_name,
+        output_file_path,
+    )
+
+    assert scoring_output_exists == True, 'Scoring output file does not exist'
+
+    print('Integration test passed')
+
+
+if __name__ == '__main__':
+    TestIntegration()
